d9190bcb0b00d4f3bad72c5c6b4ee1569bf55a54
Yura Tolstik <yltsrc@gmail.com>
2014-01-02 20:43:08 +0300

    remove code duplications

--- lib/warder/code_complexity_runner.rb
+++ lib/warder/code_complexity_runner.rb
@@ -2,15 +2,12 @@ module Warder
   # responsible for run code complexity validation
   class CodeComplexityRunner < Runner
     FLOG_SCORE = SCORE
-
-    def initialize(options = {})
-      @options = options
-    end
+    COMMAND_NAME = 'flog'
 
     private
 
-    def command
-      "flog -a -c -g -m #{@options.files}"
+    def command_with_options
+      "#{COMMAND_NAME} -a -c -g -m #{@options.files}"
     end
 
     def failed?(line)
--- lib/warder/code_duplication_runner.rb
+++ lib/warder/code_duplication_runner.rb
@@ -2,21 +2,13 @@ module Warder
   # responsible for run code duplication validation
   class CodeDuplicationRunner < Runner
     FLAY_SCORE = SCORE / 2
-
-    def initialize(options = {})
-      @options = options
-    end
+    COMMAND_NAME = 'flay'
+    FAILURE_REGEXP = /Total score \(lower is better\) = (\d+)/
 
     private
 
-    def command
-      "flay -d -m #{FLAY_SCORE} #{@options.files}"
-    end
-
-    def failed?(line)
-      match = line.match(/Total score \(lower is better\) = (\d+)/)
-
-      match && match[1].to_i != 0
+    def command_with_options
+      "#{COMMAND_NAME} -d -m #{FLAY_SCORE} #{@options.files}"
     end
   end
 end
