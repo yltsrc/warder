2751d73eca1c6da067ccb5e3e891bffac53d08ca
Yura Tolstik <yltsrc@gmail.com>
2015-07-04 13:10:27 +0300

    fix flay diff issues

--- features/step_definitions/detects_code_duplication_steps.rb
+++ features/step_definitions/detects_code_duplication_steps.rb
@@ -1,7 +1,7 @@
 def executing_code_duplication
-  "executing 'flay -d -m 15 .'"
+  "executing 'flay -m 15 .'"
 end
 
 def code_duplication_output
-  command_output_for_project_or_file('flay -d')
+  command_output_for_project_or_file('flay')
 end
